03d2931481eb32b641e5c62eae9dbe94ea1691e4
abygalersantos06-maker <abygale.rsantos06@gmail.com>
2025-11-26 14:37:22 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+<footer>
+  <p>Created by: Abygale Santos<br></p>
+  <p>Nov 2025</p>
+</footer>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>The Lumina Hairclips Store</title>
+        <link rel = "stylesheet" href = "css/styles.css">
+        <nav>
+            <a href = "do_While.php">Do while</a> |
+            <a href = "for_loop.php">For Loop</a> |
+            <a href = "ifCondition.php">If Condition</a> |
+            <a href = "ifElse.php">If Else</a> |
+            <a href = "Switch.php">Switch</a> |
+            <a href = "whileLoops.php">While Loop</a> |
+        </nav>
+    </head>
+</html>
